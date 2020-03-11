20ca97afa5f19e9ea2d03264f452f2e136b959fe
Wuxi <wuxi@wufeng-network.com>
2020-03-11 14:44:42 +0800

    [修改] 变量名及空数组

--- src/LaraScene.php
+++ src/LaraScene.php
@@ -4,7 +4,7 @@
  * @Author: uSee
  * @Date: 2020-03-05 15:53:58
  * @LastEditors: Wuxi
- * @LastEditTime: 2020-03-10 17:07:28
+ * @LastEditTime: 2020-03-11 14:43:19
  */
 
 namespace Usee\LaraScene;
@@ -12,31 +12,31 @@ namespace Usee\LaraScene;
 trait Scene
 {
     /**
-     * 所有规则
+     * 所有所有规则
      *
      * @var array
      * @Description: 
-     * @Author: uSee | wuxi889@vip.qq.com
+     * @Author: uSee | you-see@qq.com
      * @DateTime 2020-03-06
      */
-    protected $cusRules = [];
+    protected $allRules = [];
 
     /**
-     * 自定义消息
+     * 所有自定义消息
      *
      * @var array
      * @Description: 
-     * @Author: uSee | wuxi889@vip.qq.com
+     * @Author: uSee | you-see@qq.com
      * @DateTime 2020-03-06
      */
-    protected $cusMessages = [];
+    protected $allMessages = [];
 
     /**
      * 验证器场景数组
      *
      * @var array
      * @Description: 
-     * @Author: uSee | wuxi889@vip.qq.com
+     * @Author: uSee | you-see@qq.com
      * @DateTime 2020-03-06
      */
     protected $scenes = [];
@@ -46,7 +46,7 @@ trait Scene
      *
      * @var string
      * @Description: 
-     * @Author: uSee | wuxi889@vip.qq.com
+     * @Author: uSee | you-see@qq.com
      * @DateTime 2020-03-06
      */
     protected $currentScene = '';
@@ -55,32 +55,32 @@ trait Scene
      * 返回验证自定义消息
      *
      * @Description: 
-     * @Author: uSee | wuxi889@vip.qq.com
+     * @Author: uSee | you-see@qq.com
      * @DateTime 2020-03-06
      * @return array
      */
     public function messages(): array
     {
-        return $this->cusMessages;
+        return $this->allMessages;
     }
 
     /**
      * 返回验证规则
      *
      * @Description: 
-     * @Author: uSee | wuxi889@vip.qq.com
+     * @Author: uSee | you-see@qq.com
      * @DateTime 2020-03-06
      * @return array
      */
     public function rules(): array
     {
         // 获取场景下的规则名
-        $rules_name = $this->scenes[$this->currentScene];
-        $all_rules = $this->cusRules;
+        $rule_name = $this->scenes[$this->currentScene] ?? [];
+        $all_rules = $this->allRules;
 
         // 获取规则名对应的规则
         $rules = [];
-        array_walk($rules_name, function ($val) use (&$rules, $all_rules) {
+        array_walk($rule_name, function ($val) use (&$rules, $all_rules) {
             array_key_exists($val, $all_rules) && ($rules[$val] = $all_rules[$val]);
         });
 
